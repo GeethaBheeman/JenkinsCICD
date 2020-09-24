2d6443c956b5a8ddb6dbb609b9c290cac900879d
GeethaBheeman <71432566+GeethaBheeman@users.noreply.github.com>
2020-09-24 03:06:59 -0400

    Update App.java

--- src/main/java/com/edureka/App.java
+++ src/main/java/com/edureka/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello India and World!" );
+        System.out.println( "Hello webhook and World!" );
     }
 }
