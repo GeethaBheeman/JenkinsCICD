34c49fc53671d50e0e240a982ac7c0386f84d533
GeethaBheeman <71432566+GeethaBheeman@users.noreply.github.com>
2020-09-25 00:00:22 -0400

    Update App.java

--- src/main/java/com/edureka/App.java
+++ src/main/java/com/edureka/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello webhook public ip and World!" );
+        System.out.println( "test on 25!" );
     }
 }
